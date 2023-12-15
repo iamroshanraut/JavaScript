e0c6f84ff69ce534a8b13da13e27073a651bb0bf
Roshan Raut <rautroshan601@gmail.com>
2023-12-15 21:21:59 +0530

    Example 3 on flat method

--- script.js
+++ script.js
@@ -364,3 +364,44 @@ console.log(arr20.flat());
 
 const arr21 = [[[1, 2], 3], [4, [5, 6]], 7, 8];
 console.log(arr21.flat(2));
+
+//example3
+
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+const acccountMovements = accounts.map(acc => acc.movements);
+console.log(acccountMovements);
+
+const allMovements = acccountMovements.flat();
+console.log(allMovements);
+
+const sumOfMovs = allMovements.reduce((acc, movs) => acc + movs ,0);
+console.log(sumOfMovs);
